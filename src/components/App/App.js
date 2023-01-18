e983bdbe62294b16034ba98608e517f8ed35dccf
Kirill <k3499@yandex.ru>
2023-01-18 18:20:20 +0300

    final

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,42 @@
+import { useState } from "react"
 import Treat from "../Treat/Treat"
 import "./App.css"
 
 function App() {
+  const [fua, setFua] = useState({
+    name: "фуа-гра",
+    portion: 10,
+    mouses: "мышь в подарок",
+    weight: "0,5",
+    description: "Печень утки разварная с артишоками.",
+    disabled: false,
+  })
+  const [fish, setFish] = useState({
+    name: "Рыбой",
+    portion: 40,
+    mouses: "2 мыши в подарок",
+    weight: "2",
+    description: "Головы щучьи с чесноком да свежайшая сёмгушка.",
+    disabled: false,
+  })
+  const [chicken, setChicken] = useState({
+    name: "Курой",
+    portion: 100,
+    mouses: "5 мышей в подарок",
+    weight: "5",
+    description: "Филе из цыплят с трюфелями в бульоне.",
+    disabled: true,
+  })
+
   return (
     <div className="app">
-      <h1 className="app__title">Ты сегодня покормил кота?</h1>
       <div className="app__wrapper">
-        <Treat />
-        <Treat />
-        <Treat />
+        <h1 className="app__title">Ты сегодня покормил кота?</h1>
+        <div className="app__treat-box">
+          <Treat meal={fua} />
+          <Treat meal={fish} />
+          <Treat meal={chicken} />
+        </div>
       </div>
     </div>
   )
--- src/components/Treat/Treat.js
+++ src/components/Treat/Treat.js
@@ -1,32 +1,93 @@
+import { useState } from "react"
 import "./Treat.css"
 
-function Treat() {
+const Treat = ({ meal }) => {
+  const [selected, setSelected] = useState(false)
+  const [hover, setHover] = useState(false)
+  const [firstLeave, setFirstLeave] = useState(false)
+  const [available, setAvailable] = useState(meal.disabled)
+  console.log(available)
+  const select = (evt) => {
+    evt.preventDefault()
+    console.log(evt)
+    if (!selected) {
+      setSelected(true)
+    } else {
+      setSelected(false)
+    }
+  }
+
+  const handleHover = () => {
+    if (selected) {
+      if (!hover && !firstLeave) {
+        setHover(true)
+        console.log(" первый раз навел")
+      } else if (!hover && firstLeave) {
+        setHover(true)
+        console.log(" второй раз навел")
+      }
+    }
+  }
+  const handleHoverLeave = () => {
+    if (hover && !firstLeave) {
+      console.log(" первый раз покинул")
+      setFirstLeave(true)
+      setHover(false)
+    } else if (hover && firstLeave) {
+      console.log(" второй раз покинул")
+      setHover(false)
+    }
+  }
+
   return (
     <div className="treat">
-      <div className="treat__block">
+      <div
+        className={
+          "treat__block " +
+          (selected ? "selected" : "") +
+          (available ? "disabled" : "")
+        }
+        onClick={select}
+        onMouseOver={handleHover}
+        onMouseLeave={handleHoverLeave}
+      >
         <div className="treat__head-block">
-          <p className="treat__category">Сказочное заморское яство</p>
+          {hover && selected && firstLeave ? (
+            <p className="treat__category treat__category-selected">
+              Котэ не одобряет?
+            </p>
+          ) : (
+            <p className="treat__category">Сказочное заморское яство</p>
+          )}
         </div>
         <div className="treat__body-block">
           <h2 className="treat__title">Нямушка</h2>
-          <p className="treat__subtitle">с фуа-гра</p>
+          <p className="treat__subtitle">с {meal.name}</p>
           <p className="treat__features">
-            <strong>10</strong> порций
+            <strong>{meal.portion}</strong> порций
           </p>
-          <p className="treat__features">мышь в подарок</p>
+          <p className="treat__features">{meal.mouses}</p>
           <div className="treat__cat"></div>
           <div className="treat__weight-circle">
-            <p className="treat__weight">0,5</p>
+            <p className="treat__weight">{meal.weight}</p>
             <p className="treat__weight-metric">кг</p>
           </div>
         </div>
       </div>
-      <p className="treat__call">
-        Чего сидишь? Порадуй котэ,{" "}
-        <a href="#" className="treat__call-link">
-          купи.
-        </a>
-      </p>
+      {selected ? (
+        <p className="treat__call">{meal.description}</p>
+      ) : !meal.disabled ? (
+        <p className="treat__call">
+          Чего сидишь? Порадуй котэ,{" "}
+          <a href="#" className="treat__call-link" onClick={select}>
+            купи.
+          </a>
+        </p>
+      ) : (
+        <p className="treat__call treat__call-disabled">
+          Печалька, с {meal.name} закончился.
+        </p>
+      )}
     </div>
   )
 }
